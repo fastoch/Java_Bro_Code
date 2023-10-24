ed5ad811c8d64a0e07ce6846e8365f62df7767ca
fastoch <fastoch@ik.me>
2023-10-24 16:57:09 +0200

    chapter 36

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ public class Main {
 
   public static void main(String[] args) {
     
-    
+    Animal animal = new Animal();
+    animal.speak();
+
+    Dog dog = new Dog();
+    dog.speak();
   }
 }
