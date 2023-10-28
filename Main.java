90234b4bd1c8f2d6336d03c8ac732546e6af4d15
fastoch <fastoch@ik.me>
2023-10-28 15:59:14 +0200

    chapter 49

--- Main.java
+++ Main.java
@@ -1,24 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*; // not compatible with .mp3 files
 import java.util.Scanner;
 
 public class Main {
+  public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
-  public static void main(String[] args) {
+    File file = new File("/home/fastoch/Music/Takeoff.wav");
+    AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+    Clip clip = AudioSystem.getClip();
+    clip.open(audioStream);
 
-    try {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Enter a whole number to divide: ");
-      int x = scanner.nextInt();
+    Scanner scanner = new Scanner(System.in);
+    String response = ""; 
 
-      System.out.println("Enter a whole number to divide by: ");
-      int y = scanner.nextInt();
+    while(!response.equals("Q")) {
+      System.out.println("P = play, S = stop, R =reset, Q = quit");
+      response = scanner.next();
+      response = response.toUpperCase();
 
-      int z = x / y;
-      System.out.println("result: " + z);
-      
-      scanner.close();
+      switch(response) {
+        case "P":
+          clip.start();
+          break;
+        case "S":
+          clip.stop();
+          break;
+        case "R":
+          clip.setMicrosecondPosition(0);
+          break;
+        case "Q":
+          clip.close();
+          break;
+        default:
+          System.out.println("Invalid input");
+      }
     }
-    catch (ArithmeticException e) {
-      System.out.println("You can't divide by zero! IDIOT!");
-    }
+
+    scanner.close();
+    audioStream.close();
   }
 }
