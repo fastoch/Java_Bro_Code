ef47f3eb2be87e680e9c4b3da895497d8f8dd642
fastoch <fastoch@ik.me>
2023-10-29 15:46:27 +0100

    chapter 50 - labels

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Main {
   public static void main(String[] args) {
 
-    new MyFrame();
+    ImageIcon image = new ImageIcon("/home/fastoch/Pictures/profile/archlinux.png");
 
+    JLabel label = new JLabel();
+    label.setIcon(image);
+    label.setText("Danger Zone!!!");
+    label.setHorizontalTextPosition(JLabel.CENTER); // LEFT, CENTER, RIGHT of ImageIcon
+    label.setVerticalTextPosition(JLabel.BOTTOM);
+
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(500, 500);
+    frame.setVisible(true);
+    frame.add(label);
   }
 }
