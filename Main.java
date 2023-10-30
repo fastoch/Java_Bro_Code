7b3e249d19d1971a774febada0d5a97bf9e93349
fastoch <fastoch@ik.me>
2023-10-30 11:10:34 +0100

    chapter 51

--- Main.java
+++ Main.java
@@ -1,22 +1,23 @@
-import javax.swing.ImageIcon;
+import java.awt.Color;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Main {
   public static void main(String[] args) {
-
-    ImageIcon image = new ImageIcon("/home/fastoch/Pictures/profile/archlinux.png");
-
-    JLabel label = new JLabel();
-    label.setIcon(image);
-    label.setText("Danger Zone!!!");
-    label.setHorizontalTextPosition(JLabel.CENTER); // LEFT, CENTER, RIGHT of ImageIcon
-    label.setVerticalTextPosition(JLabel.BOTTOM);
+    
+    JPanel redPanel = new JPanel();
+    redPanel.setBackground(Color.RED);
+    redPanel.setBounds(0, 0, 250, 250); // top left corner
+    
+    JPanel bluePanel = new JPanel();
+    bluePanel.setBackground(Color.BLUE);
+    bluePanel.setBounds(0, 0, 250, 250); // top left corner
 
     JFrame frame = new JFrame();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(500, 500);
+    frame.setLayout(null);
+    frame.setSize(750, 750);
     frame.setVisible(true);
-    frame.add(label);
+    frame.add(redPanel);
   }
 }
