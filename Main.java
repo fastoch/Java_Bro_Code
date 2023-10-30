e71263abe617f9474a539303af9451e620323723
fastoch <fastoch@ik.me>
2023-10-30 16:32:33 +0100

    Buttons - 380min

--- Main.java
+++ Main.java
@@ -1,23 +1,7 @@
-import java.awt.Color;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 public class Main {
   public static void main(String[] args) {
     
-    JPanel redPanel = new JPanel();
-    redPanel.setBackground(Color.RED);
-    redPanel.setBounds(0, 0, 250, 250); // top left corner
-    
-    JPanel bluePanel = new JPanel();
-    bluePanel.setBackground(Color.BLUE);
-    bluePanel.setBounds(0, 0, 250, 250); // top left corner
+    new MyFrame();
 
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setLayout(null);
-    frame.setSize(750, 750);
-    frame.setVisible(true);
-    frame.add(redPanel);
   }
 }
--- MyFrame.java
+++ MyFrame.java
@@ -1,17 +1,28 @@
-import java.awt.Color;
+import java.awt.Font;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
+
+  JButton button; // make button accessible from the constructor
+
   MyFrame() {
-    this.setTitle("Arch Linux Rocks");
+
+    ImageIcon icon = new ImageIcon("point.png");
+
+    button = new JButton("Click Me");
+    button.setFont(new Font("Monospace", Font.BOLD, 16));
+    button.setBounds(200, 100, 200, 50);
+    button.addActionListener(e -> System.out.println("Lambda Expression"));
+    button.setFocusable(false); // removes the box around the text field
+    button.setIcon(icon);
+    button.setHorizontalTextPosition(JButton.LEFT);
+
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(null);
     this.setSize(600, 600);
     this.setVisible(true);
-    
-    ImageIcon image = new ImageIcon("/home/fastoch/Pictures/profile/Archer.png");
-    this.setIconImage(image.getImage());
-
-    this.getContentPane().setBackground(new Color(0x333333));
+    this.add(button);
   }
 }
