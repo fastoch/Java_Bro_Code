ad6e414ca735473dce500b757f1619a96b65b5f8
fastoch <fastoch@ik.me>
2023-11-04 15:08:33 +0100

    chapter 62

--- MyFrame.java
+++ MyFrame.java
@@ -1,38 +1,51 @@
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JRadioButton;
 import java.awt.FlowLayout;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
 
-  JButton button;
-  JCheckBox checkBox;
+  JRadioButton radioButton;
+  JRadioButton radioButton2;
+  JRadioButton radioButton3;
 
   MyFrame() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLayout(new FlowLayout());
 
-    button = new JButton("Toggle");
-    button.addActionListener(this);
+    radioButton = new JRadioButton("paëlla");
+    radioButton2 = new JRadioButton("pâtes à la bolognaise");
+    radioButton3 = new JRadioButton("Souris d'agneau");
 
-    checkBox = new JCheckBox("I'm not a robot");
-    checkBox.setFocusable(false);
-    checkBox.setFont(new Font("Consolas", Font.PLAIN, 16));
-    
-    this.add(checkBox);
-    this.add(button);
+    ButtonGroup group = new ButtonGroup();
+    group.add(radioButton);
+    group.add(radioButton2);
+    group.add(radioButton3);
+
+    radioButton.addActionListener(this);
+    radioButton2.addActionListener(this);
+    radioButton3.addActionListener(this);
+
+    this.add(radioButton);
+    this.add(radioButton2);
+    this.add(radioButton3);
+  
     this.pack(); // Frame size adjusts to fit the components you add
     this.setVisible(true);
   }
 
   @Override
-  // Toggle the state of the check box when button is clicked
   public void actionPerformed(ActionEvent e) { 
-    if(e.getSource()==button) {
-      checkBox.setSelected(!checkBox.isSelected()); 
+    if(e.getSource()==radioButton) {
+      System.out.println("You ordered paëlla");
+    }
+    else if (e.getSource()==radioButton2) {
+      System.out.println("You ordered pâtes à la bolognaise");
+    }
+    else if (e.getSource()==radioButton3) {
+      System.out.println("You ordered Souris d'agneau");
     }
   }
 }
