24cedd45e80befe6e6308d0d36aca5be35249395
fastoch <fastoch@ik.me>
2023-11-04 14:27:34 +0100

    chapter 61

--- MyFrame.java
+++ MyFrame.java
@@ -1,3 +1,4 @@
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import java.awt.FlowLayout;
@@ -7,21 +8,31 @@ import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame implements ActionListener {
 
+  JButton button;
+  JCheckBox checkBox;
+
   MyFrame() {
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLayout(new FlowLayout());
 
-    JCheckBox checkBox = new JCheckBox("I'm not a robot");
+    button = new JButton("Toggle");
+    button.addActionListener(this);
+
+    checkBox = new JCheckBox("I'm not a robot");
     checkBox.setFocusable(false);
     checkBox.setFont(new Font("Consolas", Font.PLAIN, 16));
     
     this.add(checkBox);
+    this.add(button);
     this.pack(); // Frame size adjusts to fit the components you add
     this.setVisible(true);
   }
 
   @Override
-  public void actionPerformed(ActionEvent e) {
-    
+  // Toggle the state of the check box when button is clicked
+  public void actionPerformed(ActionEvent e) { 
+    if(e.getSource()==button) {
+      checkBox.setSelected(!checkBox.isSelected()); 
+    }
   }
 }
